142413dda05a48f7901c76020c5c758c11c83c49
Ben Howkins <benhowkins03@hotmail.com>
2022-11-22 19:38:25 +0000

    Add code for scoring

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,8 +16,14 @@ function result() {
 
 function redScore() {
 
+    let oldScore = parseInt(document.getElementById('red-score').innerText);
+    document.getElementById('red-score').innerText = ++oldScore;
+
 }
 
 function blueScore () {
-    
+
+    let oldScore = parseInt(document.getElementById('blue-score').innerText);
+    document.getElementById('blue-score').innerText = ++oldScore;
+
 }
